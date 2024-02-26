5dd20b23aa5153a0cdb2e67a44c0d78f2b6a5fb7
FATHIMA-PT <fathimapt285@gmail.com>
2024-02-26 15:25:53 +0530

    Revert "home architectural designs"
    This reverts commit ff773916af58c3271730a6f690124e182a82e0eb, reversing
    changes made to 2629bb4ea034a26a3e96e5c0be4c638b5aef5e56.

--- interior-system/src/App.js
+++ interior-system/src/App.js
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+<<<<<<< HEAD
 import Home from './Components/HOME_PAGE/Home';
 import Header from './Components/COMMON/HEADER/Header';
 import Footer from './Components/COMMON/FOOTER/Footer';
 import Auth from './Components/Auth.jsx';
 
 
+=======
+import Auth from './Components/Auth';
+import Home from './Components/Home';
+>>>>>>> parent of ff77391 (home architectural designs)
 
 function App() {
   return (
@@ -16,6 +21,13 @@ function App() {
       <Route path='/Auth' element={<Auth />}/>
     </Routes>
     <Footer/>
+      <Routes>
+        <Route path='/' />
+        <Route path='/login' element={<Auth/>}/>
+        <Route path='/register' element={<Auth register/>}/>
+        <Route path='/home' element={<Home/>}/>
+
+      </Routes>
     </>
   );
 }

bd0406120ebed923fa815959807e86cf8b8f435f
FATHIMA-PT <fathimapt285@gmail.com>
2024-02-28 11:47:42 +0530

    header banner gallery

--- interior-system/src/App.js
+++ interior-system/src/App.js
@@ -1,31 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './Components/HOME_PAGE/Home';
+// import Home from './Components/HOME_PAGE/Home';
 import Header from './Components/COMMON/HEADER/Header';
 import Footer from './Components/COMMON/FOOTER/Footer';
-// import Auth from './Components/Auth.jsx';
-
 
 import Auth from './Components/Auth';
 import Home from './Components/Home';
+import Home_page from './Components/HOME_PAGE/Home_page';
+import Gallery from './Components/GALLERY/Gallery';
 
 function App() {
   return (
     <>
     <Header/>
     <Routes>
-      <Route path='/' element={<Home/>} />
-      {/* <Route path='/Auth' element={<Auth />}/> */}
+      <Route path='/' element={<Home_page/>} />
 
       <Route path='/login' element={<Auth/>}/>
         <Route path='/register' element={<Auth register/>}/>
         <Route path='/home' element={<Home/>}/>
+        <Route path='/gallery' element={<Gallery/>}/>
     </Routes>
     <Footer/>
 
-      <Route path='/Auth' element={<Auth />}/>
-    </Routes>
-    <Footer/>
+      {/* <Route path='/Auth' element={<Auth />}/> */}
+    {/* </Routes> */}
+    {/* <Footer/> */}
     </>
   );
 }

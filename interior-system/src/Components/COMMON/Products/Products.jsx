9a32671763fb981ef6fb01962b7ebd0b7fc0fb92
FATHIMA-PT <fathimapt285@gmail.com>
2024-02-29 14:19:46 +0530

    products interior

--- interior-system/src/Components/COMMON/Products/Products.jsx
+++ interior-system/src/Components/COMMON/Products/Products.jsx
@@ -1,40 +1,49 @@
 import React from "react";
-import {Button, Card } from "react-bootstrap";
-
+import chair from '../../../Assets/chairs.jpg'
+// import door from '../../../Assets/door.jpg'
+// import pairChair from '../../../Assets/vintage chairs.jpg'
+// import tile from '../../../Assets/tile.jpg'
+import './Products.css'
 
 function Products() {
   return (
     <>
-    <div className=' align-items-center justify-content-center'>
-        <h2 className="text-danger fw-bolder text-center mt-5"> Products For Sale</h2>
-          <div className="container justify-content-around mt-5">
-             <div className="row">
-                <div className="col-4 mb-5">
-                <Card className="" style={{height:'575px'}}>
-                  <Card.Img
-                    variant="top"
-                    className="img img-fluid rounded p-3"
-                    style={{ height: "265px" }}
-                    src="https://images.pexels.com/photos/3575827/pexels-photo-3575827.jpeg"
-                  />
-                  <Card.Body style={{textAlign:"justify"}} >
-                    <Card.Title className="text-center fw-bolder fs-5">
-                      Wall Tile
-                      <h5 className="mt-3" style={{textAlign:"justify"}}><span className="text-success">Description </span>: Transform your walls into a masterpiece with our stunning collection of wall tiles at Interior Harmoney.</h5>
-                      <p className="mt-3  " style={{textAlign:"justify"}}>
-                        <span className="text-success">Price</span> : $50 per Piece
-                      </p>
-                    </Card.Title>
-                    
-                <div className="text-center mt-4">
-                <Button  className="btn btn-outline-danger text-light ">BUY</Button>
-                </div>    
-                 </Card.Body>
-                </Card>
-                </div>
-             </div>
+    <div className="container mt-5 mb-5">
+      <div className="card-container">
+        <div className="card-images">
+          <img src={chair} alt="" className="card-image"  />
+          <div className="card-data">
+            <span className="card-description">CHAIR</span>
+            <h2 className="card-title"></h2>
+            <h5 className="card-price">Price:<span className="text-warning">$2000</span></h5>
+            <div className="card-button">BUY</div>
           </div>
-
+        </div>
+        {/* <div className="card-images">
+          <img src={door} alt="" className="card-image" />
+          <div className="card-data">
+            <span className="card-description">DOOR</span>
+            <h2 className="card-title"></h2>
+            <div className="card-button">BUY</div>
+          </div>
+        </div>
+        <div className="card-images">
+          <img src={pairChair} alt="" className="card-image" />
+          <div className="card-data">
+            <span className="card-description">TILE</span>
+            <h2 className="card-title"></h2>
+            <div className="card-button">BUY</div>
+          </div>
+        </div>
+        <div className="card-images">
+          <img src={tile} alt="" className="card-image" />
+          <div className="card-data">
+            <span className="card-description">TILE</span>
+            <h2 className="card-title"></h2>
+            <div className="card-button">BUY</div>
+          </div>
+        </div> */}
+      </div>
     </div>
       
     </>

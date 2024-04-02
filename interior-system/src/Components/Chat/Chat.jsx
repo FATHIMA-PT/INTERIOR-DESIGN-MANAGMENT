634098f1269b5c8cb35091aa275b6077bfe489f5
FATHIMA-PT <fathimapt285@gmail.com>
2024-04-02 10:26:05 +0530

    chat

--- interior-system/src/Components/Chat/Chat.jsx
+++ interior-system/src/Components/Chat/Chat.jsx
@@ -1,50 +1,52 @@
-import React from 'react';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
-
+import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 function Chat() {
   return (
     <>
-    
-    <Container fluid className='mt-3 mb-5 ' >
-      <Row className='' >
-        {/* Sidebar */}
-        <Col md={3} className="sidebar border shadow rounded" style={{background:'#d8c9ff'}}>
-      
-          <h3>Agent Name</h3>
-        </Col>
-        {/* Chat messages */}
-        <Col md={6} className="chat-container border shadow rounded">
-          <div className="messages">
-         
-            <div className="message incoming">
-              <p>Hello!</p>
+      <Container fluid className="mt-3 mb-5 ">
+        <Row className="">
+          {/* Sidebar */}
+          <Col
+            md={4}
+            className="sidebar border shadow rounded"
+            style={{ background: "#d8c9ff" }}
+          >
+            <h3>Agent Name</h3>
+          </Col>
+          {/* Chat messages */}
+          <Col md={8} className="chat-container border shadow rounded">
+            <div className="messages">
+              <div className="message incoming">
+                <p>Hello!</p>
+              </div>
+              <div className="message outgoing">
+                <p>Hi</p>
+              </div>
             </div>
-            <div className="message outgoing">
-              <p>Hi</p>
+            {/* Chat input form */}
+            <div className="d-flex align-items-center justify-content-between">
+              <Form className="flex-grow-1 d-flex align-items-center">
+                <Form.Group
+                  controlId="formChat"
+                  className="mb-3 flex-grow-1 me-2"
+                >
+                  <Form.Control type="text" placeholder="Type a message..." />
+                </Form.Group>
+                <button
+                  type="submit"
+                  className="btn mb-2"
+                  style={{ fontSize: "30px", color: "#1b2b03" }}
+                >
+                  <i className="fa-sharp fa-solid fa-play fa-beat"></i>
+                </button>
+              </Form>
             </div>
-            
-          </div>
-          {/* Chat input form */}
-          <Form>
-            <Form.Group controlId="formChat">
-              <Form.Control type="text" placeholder="Type a message..." />
-            </Form.Group>
-            <div className='btn mt-4 mb-3' style={{marginLeft:'650px'}}>
-            <i class="fa-sharp fa-solid fa-play fa-beat" style={{fontSize:'30px',color:'#1b2b03'}}></i>
-            </div>
-          </Form>
-        </Col>
-        {/* Users List */}
-        <Col md={3} className="user-list border rounded  shadow" style={{background:'#fff9e0'}}>
-         
-          <h3>Users List</h3>
-        </Col>
-      </Row>
-    </Container>
-   
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
